46fd143b201e4bfe3bb82df37f653aaa3ca063a4
Julian Hofer <julianhofer@gnome.org>
2025-06-13 13:46:41 +0200

    Add three tests

--- tests/integration_python/test_multi_output.py
+++ tests/integration_python/test_multi_output.py
@@ -1,10 +1,7 @@
 import shutil
 from pathlib import Path
 
-import tomli_w
-import tomllib
-
-from .common import verify_cli_command
+from .common import ExitCode, verify_cli_command
 
 
 def test_build(pixi: Path, build_data: Path, tmp_pixi_workspace: Path) -> None:
@@ -12,12 +9,13 @@ def test_build(pixi: Path, build_data: Path, tmp_pixi_workspace: Path) -> None:
     test_data = build_data.joinpath(project)
     test_data.joinpath("pixi.lock").unlink(missing_ok=True)
     shutil.copytree(test_data, tmp_pixi_workspace, dirs_exist_ok=True)
+    package_manifest = tmp_pixi_workspace.joinpath("recipe", "pixi.toml")
 
     env = {
         "PIXI_CACHE_DIR": str(tmp_pixi_workspace.joinpath("pixi_cache")),
     }
     verify_cli_command(
-        [pixi, "build", "--manifest-path", tmp_pixi_workspace, "--output-dir", tmp_pixi_workspace],
+        [pixi, "build", "--manifest-path", package_manifest, "--output-dir", tmp_pixi_workspace],
         env=env,
     )
 
@@ -32,28 +30,42 @@ def test_install(pixi: Path, build_data: Path, tmp_pixi_workspace: Path) -> None
     test_data.joinpath("pixi.lock").unlink(missing_ok=True)
     shutil.copytree(test_data, tmp_pixi_workspace, dirs_exist_ok=True)
 
-    package_manifest_path = tmp_pixi_workspace.joinpath("recipe", "pixi.toml")
-    package_manifest = tomllib.loads(package_manifest_path.read_text())
-    package_manifest["package"]["build"]["configuration"] = {"debug-dir": str(tmp_pixi_workspace)}
-    package_manifest_path.write_text(tomli_w.dumps(package_manifest))
-
     env = {
         "PIXI_CACHE_DIR": str(tmp_pixi_workspace.joinpath("pixi_cache")),
     }
     verify_cli_command([pixi, "install", "--manifest-path", tmp_pixi_workspace], env=env)
 
-    conda_metadata_params = tmp_pixi_workspace.joinpath("conda_metadata_params.json")
-    conda_build_params = tmp_pixi_workspace.joinpath("conda_build_params.json")
-
-    assert conda_metadata_params.is_file()
-    assert conda_build_params.is_file()
-
-    conda_metadata_params.unlink()
-    conda_build_params.unlink()
-
     verify_cli_command(
         [pixi, "install", "--locked", "--manifest-path", tmp_pixi_workspace], env=env
     )
 
-    assert not conda_metadata_params.is_file()
-    assert not conda_build_params.is_file()
+
+def test_available_packages(pixi: Path, build_data: Path, tmp_pixi_workspace: Path) -> None:
+    project = "multi-output"
+    test_data = build_data.joinpath(project)
+    test_data.joinpath("pixi.lock").unlink(missing_ok=True)
+    shutil.copytree(test_data, tmp_pixi_workspace, dirs_exist_ok=True)
+
+    env = {
+        "PIXI_CACHE_DIR": str(tmp_pixi_workspace.joinpath("pixi_cache")),
+    }
+
+    # foobar-desktop is a direct dependency, so it should be properly installed
+    verify_cli_command(
+        [pixi, "run", "--manifest-path", tmp_pixi_workspace, "foobar-desktop"],
+        env=env,
+        stdout_contains="Hello from foobar-desktop",
+    )
+    # foobar is a dependency of foobar-desktop, so it should be there as well
+    verify_cli_command(
+        [pixi, "run", "--manifest-path", tmp_pixi_workspace, "foobar"],
+        env=env,
+        stdout_contains="Hello from foobar",
+    )
+    # bizbar is a output of the recipe, but we don't request it
+    # So it shouldn't be part of the environment
+    verify_cli_command(
+        [pixi, "run", "--manifest-path", tmp_pixi_workspace, "bizbar"],
+        env=env,
+        expected_exit_code=ExitCode.COMMAND_NOT_FOUND,
+    )
