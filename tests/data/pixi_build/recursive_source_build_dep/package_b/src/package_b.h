aa0c4527ac9541e3992a60181f65df9deb6df596
Julian Hofer <julianhofer@gnome.org>
2025-06-27 10:12:56 +0200

    Add test

---
+++ tests/data/pixi_build/recursive_source_build_dep/package_b/src/package_b.h
@@ -0,0 +1,11 @@
+#ifndef PACKAGE_B_H
+#define PACKAGE_B_H
+
+namespace package_b {
+
+// Simple function to add two integers
+int add(int a, int b);
+
+} // namespace package_b
+
+#endif // PACKAGE_B_H
